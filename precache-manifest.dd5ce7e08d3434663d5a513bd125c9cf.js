8c0c634a770b25a3a9b85239a2c1ce6bd36010aa
lavanya <nimushakavilavanya@gmail.com>
2020-02-25 18:06:33 -0800

    Updates

---
+++ precache-manifest.dd5ce7e08d3434663d5a513bd125c9cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a027fbaffefefb6675120947c4b10ae9",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "e0a302bee94c5b4319cf",
+    "url": "/weatherapp/static/css/main.cccd85da.chunk.css"
+  },
+  {
+    "revision": "5f39bfe009cc20fa5cb1",
+    "url": "/weatherapp/static/js/2.3b54e9b1.chunk.js"
+  },
+  {
+    "revision": "8de4bdbb127f280cc6b79279dd69f244",
+    "url": "/weatherapp/static/js/2.3b54e9b1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e0a302bee94c5b4319cf",
+    "url": "/weatherapp/static/js/main.1f5f3a06.chunk.js"
+  },
+  {
+    "revision": "f248e4dc60e723a30ca5",
+    "url": "/weatherapp/static/js/runtime-main.cb22b683.js"
+  }
+]);
